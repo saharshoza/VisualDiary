34ffc1be3e4b2d9566739e201a9abfe88304bba3
Saharsh Oza <oza.saharsh@gmail.com>
2016-10-22 01:47:39 -0500

    Bug fixes in lat/long code. Better threshold and time condition

--- Code/findStationary.py
+++ Code/findStationary.py
@@ -2,19 +2,53 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
+threshold = 0.00005
+stationaryLimit = []
+
+def identify_stationary(newMat):
+	stationaryFlag = 0
+	for time in range(0,newMat.shape[0]-1):	
+		print (newMat[time][1] - newMat[time+1][1])
+		print (newMat[time][2] - newMat[time+1][2])
+		print abs(threshold)
+		if (abs(newMat[time][1] - newMat[time+1][1]) <= abs(threshold)) and (abs(newMat[time][2] - newMat[time+1][2]) <= abs(threshold)) and stationaryFlag == 0:
+			stationaryLimit.append(time)
+			stationaryFlag = 1
+		if (abs(newMat[time][1] - newMat[time+1][1]) > abs(threshold)) and (abs(newMat[time][2] - newMat[time+1][2]) > abs(threshold)) and stationaryFlag == 1:
+			stationaryLimit.append(time)
+			stationaryFlag = 0
+
 if __name__ == "__main__":
 	
 	#Get File
 	gpsIn = sys.argv[1]
 	gpsData = np.genfromtxt(gpsIn, delimiter=',')
-	#print gpsData[gpsData.shape[0]-1][0]
 	firstTimeStamp = int(gpsData[0][0])
 	newMat = np.zeros((int(gpsData[gpsData.shape[0]-1][0]) - int(gpsData[0][0]),4))
 	newMatIter = 0
 	for i in range(0,gpsData.shape[0]-1):
 		for newMatIter in range(int(gpsData[i][0]),int(gpsData[i+1][0])):
 			newMat[newMatIter - firstTimeStamp] = gpsData[i]
+			newMat[newMatIter - firstTimeStamp][0] = newMatIter
 	np.savetxt('gps_change.csv',newMat,delimiter=',')
-	plt.plot((newMat[:,0]-int(firstTimeStamp)),(newMat[:,1] - np.mean(newMat[:,1]))/np.std(newMat[:,1]))
-	plt.plot((newMat[:,0]-int(firstTimeStamp)),(newMat[:,2] - np.mean(newMat[:,2]))/np.std(newMat[:,2]))
+	#newMat[:,1] = (newMat[:,1] - np.mean(newMat[:,1]))/(newMat[newMat.shape[0]-1,1]-newMat[0,1])
+	#newMat[:,2] = (newMat[:,2] - np.mean(newMat[:,2]))/(newMat[newMat.shape[0]-1,2]-newMat[0,2])
+	newMat[:,1] = (newMat[:,1] - np.mean(newMat[:,1]))
+	newMat[:,2] = (newMat[:,2] - np.mean(newMat[:,2]))
+	identify_stationary(newMat)
+	stationaryLimitArr = np.asarray(stationaryLimit)
+	plt.plot((newMat[:,0]-int(firstTimeStamp)),newMat[:,1],label='Latitude')
+	plt.plot((newMat[:,0]-int(firstTimeStamp)),newMat[:,2],label='Longitude')
+	plt.legend()
+	print stationaryLimit
+	realStationary = []
+	for i in range(0,len(stationaryLimit)-1,2):
+		if(stationaryLimit[i+1] - stationaryLimit[i] > 100):
+			realStationary.append(stationaryLimit[i])
+			realStationary.append(stationaryLimit[i+1])
+	for xc in realStationary:
+		plt.axvline(x=xc)
+	plt.xlabel('Time')
+	plt.ylabel('GPS Data')
+	plt.grid()
 	plt.show()
